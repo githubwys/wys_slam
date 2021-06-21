58279d65ba061d8e4ac73ab6f7f4fff791f909af
wys <962401595@qq.com>
2021-06-21 16:18:16 +0800

    start

---
+++ build/CMakeFiles/match.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/match.dir/src/match.cpp.o"
+  "libmatch.pdb"
+  "libmatch.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/match.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
